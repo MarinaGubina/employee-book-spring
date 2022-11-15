dd01ef6a97fa2f820eed5a3c3f6da235845113ce
gubina <gubinamarina1998@gmail.com>
2022-11-15 16:14:51 +0300

    дз

---
+++ src/main/java/com/skypro/employee/controller/EmployeeController.java
@@ -0,0 +1,63 @@
+package com.skypro.employee.controller;
+
+import com.skypro.employee.model.Employee;
+import com.skypro.employee.record.EmployeeRequest;
+import com.skypro.employee.service.EmployeeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+/*
+HTTP методы
+* GET получение ресурса или набора ресурсов
+* POST создание ресурса
+* PUT модификация ресурса
+* PATCH частичная модификация ресурса
+* DELETE удаление ресурса
+
+ */
+
+@RestController
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/employees")
+    public Collection<Employee> getAllEmployees(){
+        return this.employeeService.getAllEmployees();
+    }
+
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody EmployeeRequest employeeRequest){
+        return this.employeeService.addEmployee(employeeRequest);
+    }
+
+    @GetMapping("/employees/salary/sum")
+    public int getSalarySum(){
+        return this.employeeService.getSalarySum();
+    }
+
+    @GetMapping("/employees/salary/min")
+    public Employee getEmployeeSalaryMin(){
+        return this.employeeService.getEmployeeSalaryMin();
+    }
+
+    @GetMapping("/employees/salary/max")
+    public Employee getEmployeeSalaryMax(){
+        return this.employeeService.getEmployeeSalaryMax();
+    }
+
+    @GetMapping("/employees/high-salary")
+    public List<Employee> getAboveAverageSalary(){
+        return this.employeeService.getAboveAverageSalary();
+    }
+
+}
